82692741e3197001e5fc238570844870c34b5858
Hanie Bp <haniebaratpoor@gmail.com>
2024-06-10 12:24:57 +0330

    change carousel

--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -48,54 +48,82 @@ const getData = async ()=> {
 const carouselConttainer = selectedElement('.carousel-inner');
 const carouselIndicators = selectedElement('.inner');
 const newArr = []
-getData().then((datas)=> {
-    console.log(datas);
-    datas.forEach((data,i)=> {
-            newArr.push(data.background_image)
-            const genre = data.genres[0].name
-            const carouselItem = createElement('div',['carousel-item','item-large','one','w-100']);
-            carouselItem.innerHTML = `
-                <div class=" d-flex align-items-center position-relative w-100">
-                <img src=${data.background_image} class='height-img-hero w-100'></img>
-                  <div class="container text-start p-5 w-50 text-hero  text-white position-absolute">
-                    <span class="text-capitalize mb-1 ps-2 fst-italic ">new release</span>
-                    <h1 class="h1 fs-1 fw-bold text-uppercase pb-4 pt-4">${data.name}
-                    </h1>
-                    <p class="width_1 fw-semibold">${genre}</p>
-                    <a href="#" class="btn btn-light" >More</a>
-                  </div>
-                </div>`
-              carouselConttainer.append(carouselItem);
 
 
-              const buttonIndicator = createElement('button');
-              buttonIndicator.innerHTML = `<img src=${data.background_image} class='height-img-hero'>`
-              buttonIndicator.type = 'button'
-              buttonIndicator.style.border = 'none'
-              buttonIndicator.style.background = 'none'
-            //   buttonIndicator.classList.add('active');
-              buttonIndicator.setAttribute('data-bs-target','#carouselDemo');
-              buttonIndicator.setAttribute('data-bs-slide-to',`${i}`);
-              carouselIndicators.append(buttonIndicator)
-           
-    });
 
-   
 
+
+
+const BaseUrl = `https://api.rawg.io/api/games?key=${apiKey}&dates=2024-06-01,2024-12-31&ordering=-released&page=1&page_size=10`
+
+
+
+async function fetchGameDetails(gameId) {
+  const url = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(`Failed to fetch details for game ID ${gameId}:`, error);
+  }
+}
+
+async function getUpcomingGamesWithDetails(i) {
+
+  
+  const games = await getData();
+  if (games) {
+    for (const game of games) {
+      const index = games.indexOf(game);
+      const gameDetails = await fetchGameDetails(game.id);
+      newArr.push(gameDetails.background_image)
+      const genre = gameDetails.genres[0].name
+      const carouselItem = createElement('div',['carousel-item','item-large','one','w-100']);
+      const link = createElement('a');
+      link.href = gameDetails.website
+      carouselItem.innerHTML = `
+          <div class=" d-flex align-items-center position-relative w-100">
+          <img src=${gameDetails.background_image} class='height-img-hero w-100'></img>
+            <div class="container text-start p-5 w-50 text-hero  text-white position-absolute">
+              <span class="text-capitalize mb-1 ps-2 fst-italic ">new release</span>
+              <h1 class="h1 fs-1 fw-bold text-uppercase pb-4 pt-4">${gameDetails.name}
+              </h1>
+              <p class="width_1 fw-semibold">${genre}</p>
+              <a href=${gameDetails.website} class="btn btn-light z-index  position-absolute" >More</a>
+            </div>
+          </div>`
+          link.append(carouselItem)
+        carouselConttainer.append(link);
+
+
+        const buttonIndicator = createElement('button');
+        buttonIndicator.innerHTML = `<img src=${gameDetails.background_image} class='height-img-hero'>`
+        buttonIndicator.type = 'button'
+        buttonIndicator.style.border = 'none'
+        buttonIndicator.style.background = 'none'
+        buttonIndicator.setAttribute('data-bs-target','#carouselDemo');
+        buttonIndicator.setAttribute('data-bs-slide-to',`${index}`);
+        carouselIndicators.append(buttonIndicator)
+     
+}
 const items = document.querySelectorAll('.carousel-item');
 console.log(items);
 Array.from(items)[0].classList.add('active');
 const buttons = document.querySelectorAll('.inner button');
-// Array.from(buttons)[0].classList.add('active')
-// buttons.forEach((el)=> {
-//     if (el.classList.contains('active')) {
-//         console.log(el);
-//         el.style.border = '1px solid blue'
-//     }
-// })
+  }
 
-})
-       
 
-       
+
+      // if (gameDetails) {
+      //   console.log(`Name: ${gameDetails.name}`);
+      //   console.log(`Released: ${gameDetails.released}`);
+      //   console.log(`Description: ${gameDetails.description_raw}`);
+      //   console.log(`Website: ${gameDetails.website}`);
+      //   console.log('---------------------------');
+      // }
+    }
+  
+
+
+getUpcomingGamesWithDetails(0)
 
