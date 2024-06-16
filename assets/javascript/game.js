8ea5b69874bf3c03335af39e0217812ba0695002
Hanie Bp <haniebaratpoor@gmail.com>
2024-06-16 09:54:06 +0330

    update page game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,90 +31,79 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+//localStorage
+class LocalStorageData {
+  static setData(data) {
+    localStorage.setItem("user", JSON.stringify(data.id));
+  }
+
+  static getData(term) {
+    const data = localStorage.getItem(term);
+    return JSON.parse(data);
+  }
+
+  static removeData() {
+    localStorage.removeItem("user");
+  }
+}
+
+///////check user
+const checkUser = async () => {
+  try {
+    const token = LocalStorageData.getData("user");
+    if (token) {
+      const res = await axios.get(
+        `https://66681ccef53957909ff69fee.mockapi.io//users/${token}`
+      );
+
+      if (res.status !== 200) {
+        window.location.replace("/pages/signin.html");
+      }
+    } else {
+      window.location.replace("/pages/signin.html");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+checkUser();
+
 ////////////////////////
 const formSelect1 = selectedElement(".form-select-1");
 const formSelect2 = selectedElement(".form-select-2");
 
-const apiKey = "6696da3ed4794d2dbcad58b3d495e0dd";
-// https://api.rawg.io/api/genres
-const apiUrl = `https://api.rawg.io/api/genres?key=${apiKey}`;
-const getData = async () => {
-  try {
-    const {
-      data: { results },
-    } = await axios.get(apiUrl);
-    // console.log(results);
-    return results;
-  } catch (error) {
-    alert(error);
-  }
-};
-
-// getData()
-
-async function fetchGameDetails(gameId) {
-  const url = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error(`Failed to fetch details for game ID ${gameId}:`, error);
-  }
-}
-
-function createOption(name, value, id) {
-  const option = createElement("option");
-  option.innerText = name;
-  option.value = value;
-  option.id = id;
-  formSelect1.append(option);
-}
-
-async function makeGenres() {
-  try {
-    const genres = await getData();
-    // console.log(genres);
-    genres.forEach((genre, i) => {
-      createOption(genre.name, i, genre.id);
-    });
-    return genres;
-  } catch (error) {
-    alert(error);
-  }
-}
-
-makeGenres();
-
 const parentGame = selectedElement(".parent-game");
-function createGame(name, bgImage, rating, platforms, summary,website) {
-  let parentPlatforms = [];
-  platforms.forEach((plat) => {
-    parentPlatforms.push(plat.platform.name);
-  });
-  console.log(parentPlatforms);
+function createGame(name, bgImage, realese, platforms, summary, website) {
+  // let parentPlatforms = [];
+  // platforms.forEach((plat) => {
+  //   parentPlatforms.push(plat.platform.name);
+  // });
+  // console.log(parentPlatforms);
   // const platform1 = platforms[0];
   // const platform2 = platforms[1];
   const img = createElement("img", ["img-game"]);
   img.src = bgImage;
   const imgWrapper = createElement("div", ["wrapper-img"], [img]);
   const p1 = createElement("p");
-  p1.innerText = name;
+  p1.innerHTML = `<span>Title: </span>${name}`;
   const p2 = createElement("p");
-  p2.innerText = rating;
+  p2.innerHTML = `<span>Realese: </span>${realese}`;
   const p3 = createElement("p");
-  p3.innerText = `Platforms: ${parentPlatforms.join(" | ")}`;
+  // p3.innerText = `Platforms: ${parentPlatforms.join(" | ")}`;
+  p3.innerHTML = `<span>Platforms: </span>${platforms}`;
   const p4 = createElement("p");
-  const description = createElement("p");
+  const description = createElement("p", ["summary"]);
   description.innerText = summary;
-  const link = createElement('div',['w-100']);
-  link.innerHTML = ` <a href=${website} class="btn btn-light" >More</a>`
+  const link = createElement("div", ["w-100"]);
+  link.innerHTML = ` <a href=${website} target="_blank" class="btn btn-outline-dark" >More</a>`;
   const informationWrapper = createElement(
     "div",
-    ["information-wrapper", "bg-info", "d-flex", "w-100", "flex-column"],
+    ["information-wrapper", "d-flex", "w-100", "flex-column"],
     [p1, p2, p3, p4]
   );
 
-  parentGame.append(imgWrapper, informationWrapper,description,link);
+  parentGame.append(imgWrapper, informationWrapper, description, link);
 }
 
 const getGenre = async (id) => {
@@ -129,43 +118,98 @@ const getGenre = async (id) => {
   }
 };
 
-function events() {
-  formSelect1.addEventListener("change", async (e) => {
-    const selectedIndex = e.target.selectedIndex;
-    const selectedOption = e.target.options[selectedIndex];
-    console.log(selectedOption.id);
-    const geners = await getGenre(selectedOption.id);
-    console.log(geners);
-    formSelect2.innerHTML = "";
-    const option1 = createElement("option");
-    option1.innerText = "Games";
-    formSelect2.append(option1);
-    geners.forEach((game, i) => {
-      // console.log(game);
-      const option2 = createElement("option");
-      option2.innerText = game.name;
-      option2.id = game.id;
-      option2.vlaue = i;
-      formSelect2.append(option2);
-    });
-  });
+///////////////////////////
+async function fetchGameDetails(gameId) {
+  const options = {
+    method: "GET",
+    url: "https://free-to-play-games-database.p.rapidapi.com/api/game",
+    params: { id: `${gameId}` },
+    headers: {
+      "x-rapidapi-key": "aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695",
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-events();
-
 formSelect2.addEventListener("change", async (e) => {
-  parentGame.innerHTML = ''
-  console.log(e.target);
+  parentGame.innerHTML = "";
+  // console.log(e.target);
   const selectedIndex = e.target.selectedIndex;
   const selectedOption = e.target.options[selectedIndex];
   const game = await fetchGameDetails(selectedOption.id);
   console.log(game);
   createGame(
-    game.name,
-    game.background_image,
-    game.rating,
-    game.parent_platforms,
-    game.description_raw,
-    game.website
+    game.title,
+    game.thumbnail,
+    game.release_date,
+    game.platform,
+    game.description,
+    game.freetogame_profile_url
   );
+});
+
+const geners = async () => {
+  const options = {
+    method: "GET",
+    url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+    params: {
+      "sort-by": "alphabetical",
+    },
+    headers: {
+      "x-rapidapi-key": "aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695",
+      "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+geners();
+
+formSelect1.addEventListener("change", async (e) => {
+  formSelect2.innerHTML = "";
+  // const option = e.target.value;
+  try {
+    const genre = e.target.value;
+    const options = {
+      method: "GET",
+      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      params: {
+        category: `${genre}`,
+      },
+      headers: {
+        "x-rapidapi-key": "aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695",
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+
+    const response = await axios.request(options);
+    const GameOption = createElement("option");
+    GameOption.innerText = "Games";
+    formSelect2.append(GameOption);
+    console.log(response.data);
+    response.data.forEach((game, i) => {
+      const option2 = createElement("option");
+      option2.innerText = game.title;
+      option2.id = game.id;
+      option2.vlaue = i;
+      formSelect2.append(option2);
+    });
+    return response;
+  } catch (error) {
+    alert(error);
+  }
 });
--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -33,43 +33,94 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 //////////////////
 
-const apiKey = "6696da3ed4794d2dbcad58b3d495e0dd";
-const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&dates=2024-01-01,2024-12-31&ordering=-added&page_size=10`;
-const getData = async () => {
-  try {
-    const {
-      data: { results },
-    } = await axios.get(apiUrl);
-    return results;
-  } catch (error) {
-    alert(error);
+// const apiKey = "6696da3ed4794d2dbcad58b3d495e0dd";
+// const apiUrl = `https://api.rawg.io/api/games?key=${apiKey}&dates=2024-01-01,2024-12-31&ordering=-added&page_size=10`;
+// const getData = async () => {
+//   try {
+//     const {
+//       data: { results },
+//     } = await axios.get(apiUrl);
+//     return results;
+//   } catch (error) {
+//     alert(error);
+//   }
+// };
+
+const options = {
+  method: 'GET',
+  url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+  params: {
+    'sort-by': 'release-date',
+    'sort-order': 'desc', // Sort by newest first
+    'page-size': 10 // Limit to 10 games
+  },
+  headers: {
+    'x-rapidapi-key': 'aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695',
+    'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
   }
 };
 
+const getData = async () => {
+  try {
+    const response = await axios.request(options);
+    const tenNewestGames = response.data.slice(0,10);
+    // console.log(tenNewestGames);
+    return tenNewestGames;
+  } catch (error) {
+    console.error('Error fetching games:', error);
+    return [];
+  }
+};
+
+getData();
 const carouselConttainer = selectedElement(".carousel-1");
 const carouselIndicators = selectedElement(".inner");
 const newArr = [];
 
-const BaseUrl = `https://api.rawg.io/api/games?key=${apiKey}&dates=2024-06-01,2024-12-31&ordering=-released&page=1&page_size=10`;
+// const BaseUrl = `https://api.rawg.io/api/games?key=${apiKey}&dates=2024-06-01,2024-12-31&ordering=-released&page=1&page_size=10`;
 
-async function fetchGameDetails(gameId) {
-  const url = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
+// async function fetchGameDetails(gameId) {
+//   const url = `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`;
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (error) {
+//     console.error(`Failed to fetch details for game ID ${gameId}:`, error);
+//   }
+// }
+
+const fetchGameDetailsById = async (gameId) => {
+  const options = {
+    method: 'GET',
+    url: 'https://free-to-play-games-database.p.rapidapi.com/api/game',
+    params: {
+      id: gameId
+    },
+    headers: {
+      'x-rapidapi-key': 'aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695',
+      'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+    }
+  };
+
   try {
-    const response = await axios.get(url);
+    const response = await axios.request(options);
     return response.data;
   } catch (error) {
-    console.error(`Failed to fetch details for game ID ${gameId}:`, error);
+    console.error(`Error fetching game details for ID ${gameId}:`, error);
+    return null; // Return null in case of error
   }
-}
+};
 
-async function getUpcomingGamesWithDetails(i) {
+async function getUpcomingGamesWithDetails() {
   const games = await getData();
   if (games) {
     for (const game of games) {
+      // console.log('game',game);
       const index = games.indexOf(game);
-      const gameDetails = await fetchGameDetails(game.id);
-      newArr.push(gameDetails.background_image);
-      const genre = gameDetails.genres[0].name;
+      const gameDetails = game
+      // await fetchGameDetailsById(game.id);
+      newArr.push(gameDetails.thumbnail);
+      // const genre = gameDetails.genres[0].name;
       const carouselItem = createElement("div", [
         "carousel-item",
         "item-large",
@@ -80,20 +131,20 @@ async function getUpcomingGamesWithDetails(i) {
       link.href = gameDetails.website;
       carouselItem.innerHTML = `
           <div class=" d-flex align-items-center position-relative w-100">
-          <img src=${gameDetails.background_image} class='height-img-hero w-100'></img>
+          <img src=${gameDetails.thumbnail} class='height-img-hero w-100'></img>
             <div class="container text-start p-5 w-50 text-hero  text-white position-absolute">
               <span class="text-capitalize mb-1 ps-2 fst-italic ">new release</span>
-              <h1 class="h1 fs-1 fw-bold text-uppercase pb-4 pt-4">${gameDetails.name}
+              <h1 class="h1 fs-1 fw-bold text-uppercase pb-4 pt-4">${gameDetails.title}
               </h1>
-              <p class="width_1 fw-semibold">${genre}</p>
-              <a href=${gameDetails.website} class="btn btn-light z-index  position-absolute" >More</a>
+              <p class="width_1 fw-semibold">${gameDetails.genre}</p>
+              <a href=${gameDetails.game_url} class="btn btn-light z-index  position-absolute" >More</a>
             </div>
           </div>`;
       link.append(carouselItem);
       carouselConttainer.append(link);
 
       const buttonIndicator = createElement("button");
-      buttonIndicator.innerHTML = `<img src=${gameDetails.background_image} class='height-img-hero'>`;
+      buttonIndicator.innerHTML = `<img src=${gameDetails.thumbnail} class='height-img-hero'>`;
       buttonIndicator.type = "button";
       buttonIndicator.style.border = "none";
       buttonIndicator.style.background = "none";
@@ -108,7 +159,7 @@ async function getUpcomingGamesWithDetails(i) {
   }
 }
 
-getUpcomingGamesWithDetails(0)
+getUpcomingGamesWithDetails()
 //////////////////////////////////////////
 
 /////////////////////////////////////////
@@ -117,17 +168,17 @@ const cardContainer = document.querySelector(".section-cards-c");
 
 const create = async (games) => {
   for (const game of games) {
-    const gameDetails = await fetchGameDetails(game.id);
-    const encodedGameName = encodeURIComponent(gameDetails.name);
+    const gameDetails = await fetchGameDetailsById(game.id);
+    const encodedGameName = encodeURIComponent(gameDetails.title);
     const playstaionUrl = `https://www.google.com/search?q=${encodedGameName}+game`;
-    const website = gameDetails.website ? gameDetails.website : playstaionUrl;
+    const website = gameDetails.game_url ? gameDetails.game_url : playstaionUrl;
     const card = createElement("div", ["d-flex", "flex-wrap", "border-none"]);
     card.innerHTML = `
                 <div class="card day border-none">
                 <div class="img-wrap">
-                  <img src=${gameDetails.background_image} class="card-img-top" alt="${gameDetails.name}"></div>
+                  <img src=${gameDetails.thumbnail} class="card-img-top" alt="${gameDetails.title}"></div>
                   <div class="card-body body-card text-white">
-                    <h5 class="card-title">${gameDetails.name}</h5>
+                    <h5 class="card-title">${gameDetails.title}</h5>
                     <a href=${website} class="btn a-link btn-light">More</a>
                   </div>
                 </div>
@@ -181,54 +232,61 @@ buttonsTabs.forEach((button) => {
 
 const getComingSoonGames = async () => {
   try {
-    const response = await axios.get("https://api.rawg.io/api/games", {
-      params: {
-        key: apiKey,
-        dates: "2024-06-11,2024-12-31",
-        ordering: "released",
-        page_size: 8,
-      },
-    });
-    console.log(response.data.results);
-    return response.data.results;
+    const response = await axios.request(options);
+    const randomGames = response.data.slice(30,38);
+    console.log(randomGames);
+    return randomGames;
   } catch (error) {
-    alert(error);
+    console.error('Error fetching random games:', error);
+    return [];
   }
 };
 
 const getNewReleases = async () => {
   try {
-    const response = await axios.get("https://api.rawg.io/api/games", {
-      params: {
-        key: apiKey,
-        dates: "2023-12-01,2024-06-10",
-        ordering: "-released",
-        page_size: 8,
-      },
-    });
-
-    console.log(response.data.results);
-    return response.data.results;
+    const response = await axios.request(options);
+    const randomGames = response.data.slice(20,28);
+    console.log(randomGames);
+    return randomGames;
   } catch (error) {
-    alert(error);
+    console.error('Error fetching random games:', error);
+    return [];
   }
 };
 
-async function getRandomGames() {
-  try {
-    const response = await axios.get("https://api.rawg.io/api/games", {
-      params: {
-        key: apiKey,
-        page_size: 8,
-        ordering: "random",
-      },
-    });
 
-    return response.data.results;
+
+
+
+const getRandomGames = async () => {
+  try {
+    const response = await axios.request(options);
+    const randomGames = response.data.slice(12,20);
+    console.log(randomGames);
+    return randomGames;
   } catch (error) {
-    alert("Error fetching games:", error);
+    console.error('Error fetching random games:', error);
+    return [];
   }
-}
+};
+
+getRandomGames();
+
+// async function getRandomGames() {
+//   try {
+//     const response = await axios.get("https://api.rawg.io/api/games", {
+//       params: {
+//         key: apiKey,
+//         page_size: 8,
+//         ordering: "random",
+//       },
+//     });
+
+//     return response.data.results;
+//   } catch (error) {
+//     alert("Error fetching games:", error);
+//   }
+// }
 
 // getRandomGames();
 
@@ -330,9 +388,10 @@ function createLi(title, image, summary, genres, shortDes, linkUrl) {
     });
   });
 }
+// https://rapidapi.com/digiwalls/api/free-to-play-games-database
 const option = {
   method: "GET",
-  url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+  url:'https://free-to-play-games-database.p.rapidapi.com/api/games',
   headers: {
     "x-rapidapi-key": "aeeb7810f6msh4f9666d654d2878p1ff652jsn543bfe84f695",
     "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
